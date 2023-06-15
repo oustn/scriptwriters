1a91916efa419cac5ed0ff6bebd95aeeb247c75a
oustn <kun.zeng.dev@gmail.com>
2023-06-15 11:39:43 +0800

    feat(init-project): init project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
+  extends: ["@scriptwriter/eslint-config"],
   settings: {
-    next: {
-      rootDir: ["apps/*/"],
-    },
   },
 };
