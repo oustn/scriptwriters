fb5e32e2d424b91aa24330c8bbfabdb55978ea1c
Zeng Kun <kun.zeng.dev@gmail.com>
2023-07-08 18:16:26 +0800

    fix: change resource to update

--- packages/cli/src/views/common/helper.ts
+++ packages/cli/src/views/common/helper.ts
@@ -1,17 +1,17 @@
 export function getRedirectUrl(payload: Record<string, string | string[]>) {
-  return `https://quantumult.app/x/open-app/add-resource?remote-resource=${JSON.stringify(
+  return `https://quantumult.app/x/open-app/update-configuration?remote-resource=${JSON.stringify(
     payload,
     null,
-    2
+    2,
   )}`;
 }
 
 export function getTaskRedirectUrl(
-  payload: Record<string, string | string[]> | string[]
+  payload: Record<string, string | string[]> | string[],
 ) {
-  return `https://quantumult.app/x/open-app/ui?module=gallery&type=task&action=add&content=${JSON.stringify(
+  return `https://quantumult.app/x/open-app/ui?module=gallery&type=task&action=update&content=${JSON.stringify(
     payload,
     null,
-    2
+    2,
   )}`;
 }
